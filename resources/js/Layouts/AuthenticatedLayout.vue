20764c1cf45a1abff83add60006336c3873458b7
Malondai <atir417@gmail.com>
2024-07-08 13:37:11 +0800

    delete theme template

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -33,6 +33,12 @@ const showingNavigationDropdown = ref(false);
                                     Dashboard
                                 </NavLink>
                             </div>
+
+                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink :href="route('user')" :active="route().current('user')">
+                                    User
+                                </NavLink>
+                            </div>
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ms-6">
--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('user')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('user');
+        Route::get('/add-user', [UserController::class, 'create'])->name('add-user');
+        Route::get('/edit-user', [UserController::class, 'edit'])->name('edit-user');
+        Route::post('/store-user', [UserController::class, 'store'])->name('store-user');
+        Route::delete('/delete-user', [UserController::class, 'destroy'])->name('delete-user');
+    });
 });
 
-Route::get('/user', [UserController::class, 'index'])->name('user');
-Route::get('/add-user', [UserController::class, 'create'])->name('add-user');
-Route::get('/edit-user', [UserController::class, 'edit'])->name('edit-user');
-Route::post('/store-user', [UserController::class, 'store'])->name('store-user');
-Route::delete('/delete-user', [UserController::class, 'destroy'])->name('delete-user');
+
 
 require __DIR__.'/auth.php';
